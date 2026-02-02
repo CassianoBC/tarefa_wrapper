d57c0296145d661540b415475f8a8933e45b4fe5
Cassiano Borges <cassianoborges@MacBook-Pro-de-Cassiano.local>
2026-02-02 18:27:13 -0300

    first

---
+++ ExercicioWrapper.java
@@ -0,0 +1,9 @@
+public class ExercicioWrapper {
+    public static void main(String args[]) {
+        int age = 22;
+        System.out.println(age);
+
+        int age2 = Integer.valueOf(12);
+        System.out.println(age2);
+    }
+}
